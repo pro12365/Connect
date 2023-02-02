6b1b03441d17655fb21fb3e2cee6572f55abc9fb
Priyanshu <priyanshurouth@gmail.com>
2023-02-02 11:44:03 +0530

    Log in and Sign Up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Chats from "./Pages/Chat";
+import "./App.css";
+import LoginForm from "./Pages/Home";
 function App() {
   return (
-    <div className="App">
-     <h1>Hello Brother Setting Up server Pleas wait for the UI</h1>
-        <h1>
-            Hi brother
-        </h1>
+    <div className="App bg-blue-500 h-screen w-screen" >
+    <Route path="/" component={LoginForm}/>
+      <Route path="/chats" component={Chats}/>
     </div>
   );
 }
--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Previews} from '@react-buddy/ide-toolbox';
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import {PaletteTree} from './palette';
+import Home from "../Pages/Home";
+import Signup from "../components/Authentication/Signup";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Home">
+                <Home/>
+            </ComponentPreview>
+            <ComponentPreview path="/Signup">
+                <Signup/>
+            </ComponentPreview>
         </Previews>
     );
 };
